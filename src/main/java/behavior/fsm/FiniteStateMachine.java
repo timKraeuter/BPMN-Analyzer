c3ce9a754e269ecb905c777d8f81616e2831928e
Tim Kräuter <tim.kraeuterr@gmail.com>
2021-11-12 11:26:35 +0100

    FSM simplified.

--- src/main/java/behavior/fsm/FiniteStateMachine.java
+++ src/main/java/behavior/fsm/FiniteStateMachine.java
@@ -7,22 +7,18 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class FiniteStateMachine implements Behavior {
-    private String name;
+    private final String name;
 
     private final State startState;
-    private final Set<State> states;
     private final Set<Transition> transitions;
 
     public FiniteStateMachine(String name, State startState) {
         this.name = name;
         this.startState = startState;
-        this.states = new LinkedHashSet<>();
         this.transitions = new LinkedHashSet<>();
     }
 
     public void addTransition(Transition transition) {
-        this.states.add(transition.getSource());
-        this.states.add(transition.getTarget());
         this.transitions.add(transition);
     }
 
