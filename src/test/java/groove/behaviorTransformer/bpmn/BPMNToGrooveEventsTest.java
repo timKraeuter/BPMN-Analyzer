a81d4c4613a3b98435cec18f95f3d92dfcea2c10
Tim Kräuter <tim.kraeuterr@gmail.com>
2022-01-21 10:34:35 +0100

    Removed Assertions.fail()

--- src/test/java/groove/behaviorTransformer/bpmn/BPMNToGrooveEventsTest.java
+++ src/test/java/groove/behaviorTransformer/bpmn/BPMNToGrooveEventsTest.java
@@ -5,7 +5,6 @@ import behavior.bpmn.activities.tasks.Task;
 import behavior.bpmn.auxiliary.BPMNCollaborationBuilder;
 import behavior.bpmn.events.*;
 import behavior.bpmn.gateways.ParallelGateway;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -92,7 +91,6 @@ class BPMNToGrooveEventsTest extends BPMNToGrooveTestBase {
                 .sequenceFlow(a, end)
                 .sequenceFlow(b, terminate_end)
                 .build();
-        Assertions.fail();
 
         this.checkGrooveGeneration(collaboration);
     }
