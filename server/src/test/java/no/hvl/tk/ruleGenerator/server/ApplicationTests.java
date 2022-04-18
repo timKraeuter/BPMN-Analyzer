5b59821796408379c7c774ca7d4264c87326d84e
Tim Kräuter <tim.kraeuterr@gmail.com>
2022-04-18 21:32:53 +0200

    Adding server base

---
+++ server/src/test/java/no/hvl/tk/ruleGenerator/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.tk.ruleGenerator.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
