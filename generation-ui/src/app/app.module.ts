adc10ddb961fc7fb38c7261eadd03a8e56a11e4e
Tim Kräuter <tim.kraeuterr@gmail.com>
2022-04-19 09:27:25 +0200

    generation ui initialized

---
+++ generation-ui/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GenerationComponent } from './generation/generation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GenerationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ generation-ui/src/app/generation/generation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generation',
+  templateUrl: './generation.component.html',
+  styleUrls: ['./generation.component.scss']
+})
+export class GenerationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
