f73951c41c45a253bf8f66fa3f57316aaafc124b
Tim Kräuter <tim.kraeuterr@gmail.com>
2022-04-26 14:40:07 +0200

    Added testcase for message events/send task without outgoing message flows.

--- generator/src/test/java/groove/behaviorTransformer/bpmn/BPMNToGrooveEventsTest.java
+++ generator/src/test/java/groove/behaviorTransformer/bpmn/BPMNToGrooveEventsTest.java
@@ -7,19 +7,35 @@ import java.io.IOException;
 class BPMNToGrooveEventsTest extends BPMNToGrooveTestBase {
 
     /**
+     * See test case <a href="https://cawemo.com/share/e6a2eb93-b0e7-4c09-baa0-93ff18084d0e">"Message Events"</a> in cawemo.
+     */
+    @Test
+    void testMessageEvents() throws IOException {
+        testGrooveGenerationForBPMNResourceFile("message-events.bpmn");
+    }
+
+    /**
+     * See test case <a href="https://cawemo.com/share/b4b588fa-3f0f-4c30-95e9-b7f5dd40cd7a">"Message Events without Message Flows"</a> in cawemo.
+     */
+    @Test
+    void testMessageEventsNoMessageFlows() throws IOException {
+        testGrooveGenerationForBPMNResourceFile("message-events-without-message-flows.bpmn");
+    }
+
+    /**
+     * See test case <a href="https://cawemo.com/share/44d74e7b-f940-48cd-8ceb-d23976b4da2b">"Two Incoming Message Flows"</a> in cawemo.
+     */
+    @Test
+    void testTwoIncomingMessageFlows() throws IOException {
+        testGrooveGenerationForBPMNResourceFile("two-incoming-message-flows.bpmn");
+    }
+
+    /**
      * See test case <a href="https://cawemo.com/share/e1777355-d0cc-45d0-8f01-87d08ba2b5ef">"Two End Events"</a> in cawemo.
      */
     @Test
     void testTwoEndEvents() throws IOException {
         testGrooveGenerationForBPMNResourceFile("two-end-events.bpmn");
-    }
-
-    /**
-     * See test case <a href="https://cawemo.com/share/519f49aa-e3ec-4d6d-8425-3933f93f974d">"Link Event"</a> in cawemo.
-     */
-    @Test
-    void testLinkEvent() throws IOException {
-        testGrooveGenerationForBPMNResourceFile("link-event.bpmn");
     }
 
     /**
@@ -31,11 +47,11 @@ class BPMNToGrooveEventsTest extends BPMNToGrooveTestBase {
     }
 
     /**
-     * See test case <a href="https://cawemo.com/share/e6a2eb93-b0e7-4c09-baa0-93ff18084d0e">"Message Events"</a> in cawemo.
+     * See test case <a href="https://cawemo.com/share/519f49aa-e3ec-4d6d-8425-3933f93f974d">"Link Event"</a> in cawemo.
      */
     @Test
-    void testMessageEvents() throws IOException {
-        testGrooveGenerationForBPMNResourceFile("message-events.bpmn");
+    void testLinkEvent() throws IOException {
+        testGrooveGenerationForBPMNResourceFile("link-event.bpmn");
     }
 
     /**
@@ -68,14 +84,6 @@ class BPMNToGrooveEventsTest extends BPMNToGrooveTestBase {
     @Test
     void testTimerEvents() throws IOException {
         testGrooveGenerationForBPMNResourceFile("timer-events.bpmn");
-    }
-
-    /**
-     * See test case <a href="https://cawemo.com/share/44d74e7b-f940-48cd-8ceb-d23976b4da2b">"Two Incoming Message Flows"</a> in cawemo.
-     */
-    @Test
-    void testTwoIncomingMessageFlows() throws IOException {
-        testGrooveGenerationForBPMNResourceFile("two-incoming-message-flows.bpmn");
     }
 
     /**
