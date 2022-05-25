2afe7a35b8e5eda2c99c53ae9d8dc24a6248fd1f
Tim Kräuter <tim.kraeuterr@gmail.com>
2022-05-25 12:55:13 +0200

    Printing progress during model checking in a separate thread.

--- generator/src/main/java/util/GrooveRunner.java
+++ generator/src/main/java/util/GrooveRunner.java
@@ -1,7 +1,6 @@
 package util;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
 public class GrooveRunner {
@@ -29,27 +28,24 @@ public class GrooveRunner {
 
         builder.redirectErrorStream(true);
         Process process = builder.start();
-        process.waitFor(60, TimeUnit.SECONDS);
         if (printOutput) {
-            printOutput(process);
+            new Thread(() -> printOutput(process)).start();
         }
+        process.waitFor(60, TimeUnit.SECONDS);
         process.destroy(); // no op if already stopped.
         process.waitFor();
         return new File(resultFilePath);
     }
 
     private void printOutput(Process p) {
-        ByteArrayOutputStream result = new ByteArrayOutputStream();
         try {
-
             byte[] buffer = new byte[1024];
             for (int length; (length = p.getInputStream().read(buffer)) != -1; ) {
-                result.write(buffer, 0, length);
+                System.out.write(buffer, 0, length);
             }
         }
         catch (IOException e) {
             System.out.println("Process output could not be read! " + e.getMessage());
         }
-        System.out.println(result.toString(StandardCharsets.UTF_8));
     }
 }
