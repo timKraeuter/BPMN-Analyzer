ceb95cc802b0405531ca0b4dfa92bfd38a95becc
Tim Kräuter <tim.kraeuterr@gmail.com>
2022-09-29 11:42:32 +0200

    Class instead of interface for constants.

--- generator/src/main/java/maude/behaviortransformer/bpmn/BPMNToMaudeTransformerConstants.java
+++ generator/src/main/java/maude/behaviortransformer/bpmn/BPMNToMaudeTransformerConstants.java
@@ -1,31 +1,35 @@
 package maude.behaviortransformer.bpmn;
 
-public interface BPMNToMaudeTransformerConstants {
-    String NEW_LINE = "\r\n    ";
-    String WHITE_SPACE = " ";
-    String PROCESSES = "processes";
-    String BPMN_SYSTEM = "BPMNSystem";
+public final class BPMNToMaudeTransformerConstants {
+    private BPMNToMaudeTransformerConstants() {
+        // Only constants
+    }
 
-    String PS = "PS";
-    String ANY_PROCESS = "P";
-    String ANY_TOKENS = "T";
-    String ANY_SIGNALS = "SIG";
-    String ANY_SUBPROCESSES = "S";
-    String ANY_MESSAGES = "M";
-    String ANY_OTHER_TOKENS = WHITE_SPACE + ANY_TOKENS;
-    String ANY_OTHER_SIGNALS = WHITE_SPACE + ANY_SIGNALS;
-    String ANY_OTHER_SUBPROCESSES = WHITE_SPACE + ANY_SUBPROCESSES;
-    String ANY_OTHER_MESSAGES = WHITE_SPACE + ANY_MESSAGES;
-    String ANY_OTHER_PROCESSES = WHITE_SPACE + ANY_PROCESS;
-    String NONE = "none";
-    String RULE_NAME_NAME_ID_FORMAT = "%s_%s";
-    String RULE_NAME_ID_FORMAT = "%s";
-    String TOKEN_FORMAT = "\"%s (%s)\""; // Name of the FlowElement followed by id.
-    String SIGNAL_OCCURENCE_FORMAT = "\"%s (%s)_signal\"";
-    String TOKEN_FORMAT_ONLY_ID = "\"%s\""; // Name of the FlowElement followed by id.
-    String SIGNAL_OCCURENCE_FORMAT_ONLY_ID = "\"%s_signal\""; // Name of the FlowElement followed by id.
-    String ENQUOTE_FORMAT = "\"%s\""; // Id of the FlowElement.
-    String BRACKET_FORMAT = "(%s)";
-    String RUNNING = "Running";
-    String TERMINATED = "Terminated";
+    public static final String NEW_LINE = "\r\n    ";
+    public static final String WHITE_SPACE = " ";
+    public static final String PROCESSES = "processes";
+    public static final String BPMN_SYSTEM = "BPMNSystem";
+
+    public static final String PS = "PS";
+    public static final String ANY_PROCESS = "P";
+    public static final String ANY_TOKENS = "T";
+    public static final String ANY_SIGNALS = "SIG";
+    public static final String ANY_SUBPROCESSES = "S";
+    public static final String ANY_MESSAGES = "M";
+    public static final String ANY_OTHER_TOKENS = WHITE_SPACE + ANY_TOKENS;
+    public static final String ANY_OTHER_SIGNALS = WHITE_SPACE + ANY_SIGNALS;
+    public static final String ANY_OTHER_SUBPROCESSES = WHITE_SPACE + ANY_SUBPROCESSES;
+    public static final String ANY_OTHER_MESSAGES = WHITE_SPACE + ANY_MESSAGES;
+    public static final String ANY_OTHER_PROCESSES = WHITE_SPACE + ANY_PROCESS;
+    public static final String NONE = "none";
+    public static final String RULE_NAME_NAME_ID_FORMAT = "%s_%s";
+    public static final String RULE_NAME_ID_FORMAT = "%s";
+    public static final String TOKEN_FORMAT = "\"%s (%s)\""; // Name of the FlowElement followed by id.
+    public static final String SIGNAL_OCCURENCE_FORMAT = "\"%s (%s)_signal\"";
+    public static final String TOKEN_FORMAT_ONLY_ID = "\"%s\""; // Name of the FlowElement followed by id.
+    public static final String SIGNAL_OCCURENCE_FORMAT_ONLY_ID = "\"%s_signal\""; // Name of the FlowElement followed by id.
+    public static final String ENQUOTE_FORMAT = "\"%s\""; // Id of the FlowElement.
+    public static final String BRACKET_FORMAT = "(%s)";
+    public static final String RUNNING = "Running";
+    public static final String TERMINATED = "Terminated";
 }
