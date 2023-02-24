e2d5d1dd2f2774ea75477b1bc94a6f4bd6979af1
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-02-24 15:52:23 +0100

    Added main class to generate gg from jar in the future

---
+++ generator/src/main/java/groove/behaviortransformer/bpmn/BPMNTransformerDriver.java
@@ -0,0 +1,8 @@
+package groove.behaviortransformer.bpmn;
+
+public class BPMNTransformerDriver {
+
+  public static void main(String[] args) {
+    // TODO: Generate a graph grammar for one ore more bpmn files (args).
+  }
+}
