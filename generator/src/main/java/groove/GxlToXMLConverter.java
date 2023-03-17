01a83d8b2458252fbbe07b18e90aec3bc30520e0
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-03-17 13:27:36 +0100

    Fix concurrent JAXB bug

--- generator/src/main/java/groove/GxlToXMLConverter.java
+++ generator/src/main/java/groove/GxlToXMLConverter.java
@@ -11,16 +11,14 @@ import java.io.StringWriter;
 
 public class GxlToXMLConverter {
 
-  private static JAXBContext jaxbContext;
-  private static Marshaller jaxbMarshaller;
-
   private GxlToXMLConverter() {}
 
   public static void toXml(final Gxl gxl, File outputFile) {
-    createJAXBObjectsIfNeeded();
     try {
       outputFile.getParentFile().mkdirs();
       outputFile.createNewFile();
+
+      Marshaller jaxbMarshaller = createJAXBMarshaller();
       jaxbMarshaller.marshal(gxl, outputFile);
     } catch (final JAXBException | IOException e) {
       throw new ShouldNotHappenRuntimeException(e);
@@ -28,13 +26,13 @@ public class GxlToXMLConverter {
   }
 
   public static String toXml(final Gxl graph) {
-    createJAXBObjectsIfNeeded();
     return marshallGraph(graph);
   }
 
   private static String marshallGraph(final Gxl graph) {
     final StringWriter sw = new StringWriter();
     try {
+      Marshaller jaxbMarshaller = createJAXBMarshaller();
       jaxbMarshaller.marshal(graph, sw);
     } catch (final JAXBException e) {
       throw new ShouldNotHappenRuntimeException(e);
@@ -42,15 +40,11 @@ public class GxlToXMLConverter {
     return sw.toString();
   }
 
-  private static void createJAXBObjectsIfNeeded() {
-    if (jaxbContext == null) {
-      try {
-        jaxbContext = JAXBContext.newInstance(Gxl.class);
-        jaxbMarshaller = jaxbContext.createMarshaller();
-        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-      } catch (final JAXBException e) {
-        throw new ShouldNotHappenRuntimeException(e);
-      }
-    }
+  private static Marshaller createJAXBMarshaller() throws JAXBException {
+    JAXBContext jaxbContext = JAXBContext.newInstance(Gxl.class);
+    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+    return jaxbMarshaller;
   }
 }
