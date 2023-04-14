3cad0fa9f3aa041bbe32e33dc79f06686ae61a53
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-04-14 14:24:25 +0200

    Use text block

--- generator/src/main/java/maude/behaviortransformer/bpmn/BPMNToMaudeTransformer.java
+++ generator/src/main/java/maude/behaviortransformer/bpmn/BPMNToMaudeTransformer.java
@@ -24,172 +24,148 @@ public class BPMNToMaudeTransformer implements BPMNToMaudeTransformerHelper {
   private final MaudeObjectBuilder objectBuilder;
 
   private static final String MODULE_TEMPLATE =
-      "load model-checker.maude .\r\n"
-          + "\r\n"
-          + "--- Multiset implementation could be extracted as well.\r\n"
-          + "fmod MSET is\r\n"
-          + "    pr STRING .\r\n"
-          + "    pr NAT .\r\n"
-          + "    sorts NeMSet MSet .\r\n"
-          + "    subsort String < NeMSet < MSet .\r\n"
-          + "\r\n"
-          + "    op none : -> MSet [ctor] .\r\n"
-          + "    op __ : MSet MSet -> MSet [ctor assoc comm id: none] .\r\n"
-          + "    op __ : NeMSet MSet -> NeMSet [ctor ditto] .\r\n"
-          + "    op __ : MSet NeMSet -> NeMSet [ctor ditto] .\r\n"
-          + "\r\n"
-          + "    op contains : MSet String -> Bool .\r\n"
-          + "\r\n"
-          + "    var X : String .\r\n"
-          + "    var Y : String .\r\n"
-          + "    var S S1 : MSet .\r\n"
-          + "\r\n"
-          + "    eq contains(none, X) = false .\r\n"
-          + "    eq contains(X S, X) = true .\r\n"
-          + "    ceq contains(Y S, X) = contains(S, X) if X =/= Y .\r\n"
-          + "\r\n"
-          + "    op size : MSet -> Nat .\r\n"
-          + "    eq size(none) = 0 .\r\n"
-          + "    eq size(X S) = size(S) + 1 .\r\n"
-          + "\r\n"
-          + "    op lessOrEqualSize : MSet MSet -> Bool .\r\n"
-          + "    ceq lessOrEqualSize(S, S1) = true if size(S) <= size(S1) .\r\n"
-          + "    eq lessOrEqualSize(S, S1) = false [owise] .\r\n"
-          + "endfm\r\n"
-          + "\r\n"
-          + "mod BPMN-EXECUTION is\r\n"
-          + "    pr MSET .\r\n"
-          + "    pr STRING .\r\n"
-          + "    pr CONFIGURATION .\r\n"
-          + "\r\n"
-          + "    sort ProcessState .\r\n"
-          + "    subsort String < Oid .\r\n"
-          + "\r\n"
-          + "    --- BPMNSystem\r\n"
-          + "    op BPMNSystem : -> Cid [ctor] .\r\n"
-          + "    op messages :_ : MSet -> Attribute [ctor] .\r\n"
-          + "    op processes :_ : Configuration -> Attribute [ctor] .\r\n"
-          + "\r\n"
-          + "    --- Processes\r\n"
-          + "    op ProcessSnapshot : -> Cid [ctor] .\r\n"
-          + "    op name :_ : String -> Attribute [ctor] .\r\n"
-          + "    op tokens :_ : MSet -> Attribute [ctor] .\r\n"
-          + "    op signals :_ : MSet -> Attribute [ctor] .\r\n"
-          + "    op subprocesses :_ : Configuration -> Attribute [ctor] .\r\n"
-          + "    ops Running, Terminated : -> ProcessState [ctor] .\r\n"
-          + "    op state :_ : ProcessState -> Attribute [ctor] .\r\n"
-          + "\r\n"
-          + "    --- Auxiliary\r\n"
-          + "    op signalAll : Configuration MSet -> Configuration .\r\n"
-          + "    op signal : MSet MSet -> MSet .\r\n"
-          + "    op terminate : Configuration -> Configuration .\r\n"
-          + "\r\n"
-          + "    var X : Oid .\r\n"
-          + "    vars P P1 : String .\r\n"
-          + "    vars T T1 : MSet . --- tokens\r\n"
-          + "    vars SIG : MSet . --- signals\r\n"
-          + "    vars S : Configuration . --- subprocesses\r\n"
-          + "    vars STATE : ProcessState . --- state\r\n"
-          + "    var PS : Configuration .\r\n"
-          + "\r\n"
-          + "    eq signalAll(none, T) = none .\r\n"
-          + "    ceq signalAll(< X : ProcessSnapshot | name : P, tokens : T, "
-          + "signals : SIG, subprocesses : S, state : STATE > PS, T1) = < X : "
-          + "ProcessSnapshot | name : P, tokens : T, signals : (SIG signal(T, "
-          + "T1)), subprocesses : signalAll(S, T1), state : STATE > signalAll"
-          + "(PS, T1) if lessOrEqualSize(SIG, T1). --- maximum signal bound is "
-          + "the number of tokens.\r\n"
-          + "    eq signalAll(< X : ProcessSnapshot | name : P, tokens : T, "
-          + "signals : SIG, subprocesses : S, state : STATE > PS, T1) = < X : "
-          + "ProcessSnapshot | name : P, tokens : T, signals : (SIG), "
-          + "subprocesses : signalAll(S, T1), state : STATE > signalAll(PS, T1)"
-          + " [owise] .\r\n"
-          + "\r\n"
-          + "    ceq signal(P T, T1) = (P + \"_signal\") signal(T, T1) if "
-          + "contains(T1, P) .\r\n"
-          + "    eq signal(P T, T1) = signal(T, T1) [owise] .\r\n"
-          + "    eq signal(none, T1) = none .\r\n"
-          + "\r\n"
-          + "    eq terminate(none) = none .\r\n"
-          + "    --- NOOP if already terminated\r\n"
-          + "    eq terminate(< X : ProcessSnapshot | name : P, tokens : T, "
-          + "signals : SIG, subprocesses : S, state : Terminated >) = < X : "
-          + "ProcessSnapshot | name : P, tokens : T, signals : SIG, "
-          + "subprocesses : S, state : Terminated > .\r\n"
-          + "    --- Terminate all subprocesses recursively\r\n"
-          + "    eq terminate(< X : ProcessSnapshot | name : P, tokens : T, "
-          + "signals : SIG, subprocesses : S, state : STATE > PS) = < X : "
-          + "ProcessSnapshot | name : P, tokens : T, signals : SIG, "
-          + "subprocesses : terminate(S), state : Terminated > terminate(PS) "
-          + ".\r\n"
-          + "\r\n"
-          + "    rl [naturalTerminate] :\r\n"
-          + "    < X : ProcessSnapshot | name : P, tokens : none, signals : "
-          + "SIG, subprocesses : none, state : Running >\r\n"
-          + "                            =>\r\n"
-          + "    < X : ProcessSnapshot | name : P, tokens : none, signals : "
-          + "none, subprocesses : none, state : Terminated > .\r\n"
-          + "endm\r\n"
-          + "\r\n"
-          + "mod BPMN-EXECUTION-${name} is\r\n"
-          + "    pr BPMN-EXECUTION .\r\n"
-          + "\r\n"
-          + "    --- Generated variables\r\n"
-          + "    ${vars}\r\n"
-          + "\r\n"
-          + "    --- Generated rules\r\n"
-          + "    ${rules}\r\n"
-          + "\r\n"
-          + "    --- Start configuration which would be generated\r\n"
-          + "    op init : -> Configuration .\r\n"
-          + "    eq init = ${init} .\r\n"
-          + "endm\r\n"
-          + "\r\n"
-          + "mod BPMN-PREDS is\r\n"
-          + "    pr BPMN-EXECUTION-${name} .\r\n"
-          + "    pr SATISFACTION .\r\n"
-          + "    subsort Configuration < State .\r\n"
-          + "\r\n"
-          + "    var C : Configuration .\r\n"
-          + "    var P : Prop .\r\n"
-          + "    var X Y : Oid .\r\n"
-          + "    var N : String . --- name\r\n"
-          + "    var T : MSet . --- tokens\r\n"
-          + "    var SIG : MSet . --- signals\r\n"
-          + "    var M : MSet . --- messages\r\n"
-          + "    var T1 : NeMSet .\r\n"
-          + "    var S : Configuration .\r\n"
-          + "    var State : ProcessState .\r\n"
-          + "\r\n"
-          + "\r\n"
-          + "    op allTerminated : -> Prop .\r\n"
-          + "    eq < X : BPMNSystem | messages : M, processes : (< Y : "
-          + "ProcessSnapshot | name : N, tokens : T, signals : SIG, "
-          + "subprocesses : S, state : Running > C) > |= allTerminated = false "
-          + ".\r\n"
-          + "    eq < X : BPMNSystem | messages : M, processes : (C) > |= "
-          + "allTerminated = true [owise] .\r\n"
-          + "\r\n"
-          + "    op unsafe : -> Prop .\r\n"
-          + "    eq < X : BPMNSystem | messages : M, processes : (< Y : "
-          + "ProcessSnapshot | name : N, tokens : (T1 T1 T), signals : SIG, "
-          + "subprocesses : S, state : State > C) > |= unsafe = true .\r\n"
-          + "    eq < X : BPMNSystem | messages : M, processes : (C) > |= "
-          + "unsafe = false [owise] .\r\n"
-          + "\r\n"
-          + "    --- Generated atomic propositions\r\n"
-          + "    ${atomicPropositions}\r\n"
-          + "endm\r\n"
-          + "\r\n"
-          + "mod BPMN-CHECK is\r\n"
-          + "    pr BPMN-PREDS .\r\n"
-          + "    pr MODEL-CHECKER .\r\n"
-          + "    pr LTL-SIMPLIFIER .\r\n"
-          + "\r\n"
-          + "    var X : Configuration .\r\n"
-          + "endm\r\n"
-          + "\r\n"
-          + "${finalQuery} .\r\n";
+      """
+load model-checker.maude .\r
+\r
+--- Multiset implementation could be extracted as well.\r
+fmod MSET is\r
+    pr STRING .\r
+    pr NAT .\r
+    sorts NeMSet MSet .\r
+    subsort String < NeMSet < MSet .\r
+\r
+    op none : -> MSet [ctor] .\r
+    op __ : MSet MSet -> MSet [ctor assoc comm id: none] .\r
+    op __ : NeMSet MSet -> NeMSet [ctor ditto] .\r
+    op __ : MSet NeMSet -> NeMSet [ctor ditto] .\r
+\r
+    op contains : MSet String -> Bool .\r
+\r
+    var X : String .\r
+    var Y : String .\r
+    var S S1 : MSet .\r
+\r
+    eq contains(none, X) = false .\r
+    eq contains(X S, X) = true .\r
+    ceq contains(Y S, X) = contains(S, X) if X =/= Y .\r
+\r
+    op size : MSet -> Nat .\r
+    eq size(none) = 0 .\r
+    eq size(X S) = size(S) + 1 .\r
+\r
+    op lessOrEqualSize : MSet MSet -> Bool .\r
+    ceq lessOrEqualSize(S, S1) = true if size(S) <= size(S1) .\r
+    eq lessOrEqualSize(S, S1) = false [owise] .\r
+endfm\r
+\r
+mod BPMN-EXECUTION is\r
+    pr MSET .\r
+    pr STRING .\r
+    pr CONFIGURATION .\r
+\r
+    sort ProcessState .\r
+    subsort String < Oid .\r
+\r
+    --- BPMNSystem\r
+    op BPMNSystem : -> Cid [ctor] .\r
+    op messages :_ : MSet -> Attribute [ctor] .\r
+    op processes :_ : Configuration -> Attribute [ctor] .\r
+\r
+    --- Processes\r
+    op ProcessSnapshot : -> Cid [ctor] .\r
+    op name :_ : String -> Attribute [ctor] .\r
+    op tokens :_ : MSet -> Attribute [ctor] .\r
+    op signals :_ : MSet -> Attribute [ctor] .\r
+    op subprocesses :_ : Configuration -> Attribute [ctor] .\r
+    ops Running, Terminated : -> ProcessState [ctor] .\r
+    op state :_ : ProcessState -> Attribute [ctor] .\r
+\r
+    --- Auxiliary\r
+    op signalAll : Configuration MSet -> Configuration .\r
+    op signal : MSet MSet -> MSet .\r
+    op terminate : Configuration -> Configuration .\r
+\r
+    var X : Oid .\r
+    vars P P1 : String .\r
+    vars T T1 : MSet . --- tokens\r
+    vars SIG : MSet . --- signals\r
+    vars S : Configuration . --- subprocesses\r
+    vars STATE : ProcessState . --- state\r
+    var PS : Configuration .\r
+\r
+    eq signalAll(none, T) = none .\r
+    ceq signalAll(< X : ProcessSnapshot | name : P, tokens : T, signals : SIG, subprocesses : S, state : STATE > PS, T1) = < X : ProcessSnapshot | name : P, tokens : T, signals : (SIG signal(T, T1)), subprocesses : signalAll(S, T1), state : STATE > signalAll(PS, T1) if lessOrEqualSize(SIG, T1). --- maximum signal bound is the number of tokens.\r
+    eq signalAll(< X : ProcessSnapshot | name : P, tokens : T, signals : SIG, subprocesses : S, state : STATE > PS, T1) = < X : ProcessSnapshot | name : P, tokens : T, signals : (SIG), subprocesses : signalAll(S, T1), state : STATE > signalAll(PS, T1) [owise] .\r
+\r
+    ceq signal(P T, T1) = (P + "_signal") signal(T, T1) if contains(T1, P) .\r
+    eq signal(P T, T1) = signal(T, T1) [owise] .\r
+    eq signal(none, T1) = none .\r
+\r
+    eq terminate(none) = none .\r
+    --- NOOP if already terminated\r
+    eq terminate(< X : ProcessSnapshot | name : P, tokens : T, signals : SIG, subprocesses : S, state : Terminated >) = < X : ProcessSnapshot | name : P, tokens : T, signals : SIG, subprocesses : S, state : Terminated > .\r
+    --- Terminate all subprocesses recursively\r
+    eq terminate(< X : ProcessSnapshot | name : P, tokens : T, signals : SIG, subprocesses : S, state : STATE > PS) = < X : ProcessSnapshot | name : P, tokens : T, signals : SIG, subprocesses : terminate(S), state : Terminated > terminate(PS) .\r
+\r
+    rl [naturalTerminate] :\r
+    < X : ProcessSnapshot | name : P, tokens : none, signals : SIG, subprocesses : none, state : Running >\r
+                            =>\r
+    < X : ProcessSnapshot | name : P, tokens : none, signals : none, subprocesses : none, state : Terminated > .\r
+endm\r
+\r
+mod BPMN-EXECUTION-${name} is\r
+    pr BPMN-EXECUTION .\r
+\r
+    --- Generated variables\r
+    ${vars}\r
+\r
+    --- Generated rules\r
+    ${rules}\r
+\r
+    --- Start configuration which would be generated\r
+    op init : -> Configuration .\r
+    eq init = ${init} .\r
+endm\r
+\r
+mod BPMN-PREDS is\r
+    pr BPMN-EXECUTION-${name} .\r
+    pr SATISFACTION .\r
+    subsort Configuration < State .\r
+\r
+    var C : Configuration .\r
+    var P : Prop .\r
+    var X Y : Oid .\r
+    var N : String . --- name\r
+    var T : MSet . --- tokens\r
+    var SIG : MSet . --- signals\r
+    var M : MSet . --- messages\r
+    var T1 : NeMSet .\r
+    var S : Configuration .\r
+    var State : ProcessState .\r
+\r
+\r
+    op allTerminated : -> Prop .\r
+    eq < X : BPMNSystem | messages : M, processes : (< Y : ProcessSnapshot | name : N, tokens : T, signals : SIG, subprocesses : S, state : Running > C) > |= allTerminated = false .\r
+    eq < X : BPMNSystem | messages : M, processes : (C) > |= allTerminated = true [owise] .\r
+\r
+    op unsafe : -> Prop .\r
+    eq < X : BPMNSystem | messages : M, processes : (< Y : ProcessSnapshot | name : N, tokens : (T1 T1 T), signals : SIG, subprocesses : S, state : State > C) > |= unsafe = true .\r
+    eq < X : BPMNSystem | messages : M, processes : (C) > |= unsafe = false [owise] .\r
+\r
+    --- Generated atomic propositions\r
+    ${atomicPropositions}\r
+endm\r
+\r
+mod BPMN-CHECK is\r
+    pr BPMN-PREDS .\r
+    pr MODEL-CHECKER .\r
+    pr LTL-SIMPLIFIER .\r
+\r
+    var X : Configuration .\r
+endm\r
+\r
+${finalQuery} .\r
+""";
 
   public BPMNToMaudeTransformer(
       BPMNCollaboration collaboration, MaudeBPMNGenerationSettings settings) {
