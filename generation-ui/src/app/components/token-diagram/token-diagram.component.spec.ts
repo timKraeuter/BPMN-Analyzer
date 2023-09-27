4ccf854c1fbd871131ac06a495ca0258c390ab6d
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-09-27 14:47:26 +0200

    Cleanup

--- generation-ui/src/app/components/token-diagram/token-diagram.component.spec.ts
+++ generation-ui/src/app/components/token-diagram/token-diagram.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TokenDiagramComponent } from './token-diagram.component';
 
 describe('TokenDiagramComponent', () => {
-  let component: TokenDiagramComponent;
-  let fixture: ComponentFixture<TokenDiagramComponent>;
+    let component: TokenDiagramComponent;
+    let fixture: ComponentFixture<TokenDiagramComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [TokenDiagramComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [TokenDiagramComponent],
+        });
+        fixture = TestBed.createComponent(TokenDiagramComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(TokenDiagramComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
