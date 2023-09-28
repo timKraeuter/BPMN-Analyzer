2c7d044e03c7287dfe98d7e19b192aabab4c5294
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-09-28 23:32:52 +0200

    Start implementing switch and highlight

--- generation-ui/src/app/pages/proposition/proposition.component.ts
+++ generation-ui/src/app/pages/proposition/proposition.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BPMNModelerService } from '../../services/bpmnmodeler.service';
 
 @Component({
     selector: 'app-process-state',
@@ -6,34 +7,43 @@ import { Component } from '@angular/core';
     styleUrls: ['./proposition.component.scss'],
 })
 export class PropositionComponent {
-    public propositions: ProcessState[] = [
-        {
-            name: 'Photos',
-            updated: new Date('1/1/16'),
-            xml: '',
-        },
-        {
-            name: 'Recipes',
-            updated: new Date('1/17/16'),
-            xml: '',
-        },
-        {
-            name: 'Work',
-            updated: new Date('1/28/16'),
-            xml: '',
-        },
-    ];
+    public currentProposition: Proposition = {
+        name: 'First proposition',
+        updated: new Date(),
+        xml: '',
+    };
+    public propositions: Proposition[] = [this.currentProposition];
 
-    newProcessState() {
-        this.propositions.push({
+    constructor(private modeler: BPMNModelerService) {}
+
+    createNewProposition() {
+        const newProposition = {
             name: 'newProposition',
             updated: new Date(),
             xml: '',
-        });
+        };
+        this.propositions.push(newProposition);
+        this.switchAndSaveAndLoadXML(newProposition);
+    }
+
+    private switchAndSaveAndLoadXML(newProposition: {
+        xml: string;
+        name: string;
+        updated: Date;
+    }) {
+        // TODO: Save xml of old proposition
+        this.currentProposition = newProposition;
+        // TODO: Load plain xml from modeler
+    }
+
+    switchToProposition(proposition: Proposition) {
+        if (proposition !== this.currentProposition) {
+            this.switchAndSaveAndLoadXML(proposition);
+        }
     }
 }
 
-export interface ProcessState {
+export interface Proposition {
     name: string;
     updated: Date;
     xml: string;
