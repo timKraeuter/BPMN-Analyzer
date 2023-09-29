e1a49d93d8068c94e5bbea7a8628d71d0bdcd64c
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-09-30 00:17:53 +0200

    Remove equals and hashcode in record

--- server/src/main/java/no/tk/rulegenerator/server/endpoint/dtos/BPMNPropertyCheckingResult.java
+++ server/src/main/java/no/tk/rulegenerator/server/endpoint/dtos/BPMNPropertyCheckingResult.java
@@ -1,7 +1,5 @@
 package no.tk.rulegenerator.server.endpoint.dtos;
 
-import java.util.Objects;
-
 public record BPMNPropertyCheckingResult(
     BPMNSpecificProperty name, boolean valid, String additionalInfo)
     implements Comparable<BPMNPropertyCheckingResult> {
@@ -9,23 +7,5 @@ public record BPMNPropertyCheckingResult(
   @Override
   public int compareTo(BPMNPropertyCheckingResult o) {
     return Integer.compare(this.name().getOrdering(), o.name().getOrdering());
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (!(o instanceof BPMNPropertyCheckingResult that)) {
-      return false;
-    }
-    return valid == that.valid
-        && name == that.name
-        && Objects.equals(additionalInfo, that.additionalInfo);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, valid, additionalInfo);
   }
 }
