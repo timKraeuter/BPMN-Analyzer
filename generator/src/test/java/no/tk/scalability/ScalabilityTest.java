48524bcb3b8eaf0cfd7228ac217bc558780890c7
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-02-27 14:20:06 +0100

    Generate models with different branch length

--- generator/src/test/java/no/tk/scalability/ScalabilityTest.java
+++ generator/src/test/java/no/tk/scalability/ScalabilityTest.java
@@ -6,65 +6,73 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
+
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.bpm.model.bpmn.instance.FlowElement;
 import org.junit.jupiter.api.Test;
 
 class ScalabilityTest {
 
-  @Test
-  void block1Test() {
-    BpmnModelInstance modelInstance = new BPMNModelBuilder().block1().buildWithEndEvent();
+    @Test
+    void block1Test() {
+        BpmnModelInstance modelInstance = new BPMNModelBuilder().block1().buildWithEndEvent();
 
-    assertThat(modelInstance.getModelElementsByType(FlowElement.class).size(), is(9));
-  }
+        assertThat(modelInstance.getModelElementsByType(FlowElement.class).size(), is(9));
+    }
 
-  @Test
-  void block2Test() {
-    BpmnModelInstance modelInstance = new BPMNModelBuilder().block2().buildWithEndEvent();
+    @Test
+    void block2Test() {
+        BpmnModelInstance modelInstance = new BPMNModelBuilder().block2().buildWithEndEvent();
 
-    assertThat(modelInstance.getModelElementsByType(FlowElement.class).size(), is(12));
-  }
+        assertThat(modelInstance.getModelElementsByType(FlowElement.class).size(), is(12));
+    }
 
-  @Test
-  void block3Test() {
-    BpmnModelInstance modelInstance = new BPMNModelBuilder().block3().buildWithEndEvent();
+    @Test
+    void block3Test() {
+        BpmnModelInstance modelInstance = new BPMNModelBuilder().block3().buildWithEndEvent();
 
-    assertThat(modelInstance.getModelElementsByType(FlowElement.class).size(), is(12));
-  }
+        assertThat(modelInstance.getModelElementsByType(FlowElement.class).size(), is(12));
+    }
 
-  @Test
-  void blockChainingTest() {
-    BpmnModelInstance modelInstance =
-        new BPMNModelBuilder().block1().block2().block3().buildWithEndEvent();
+    @Test
+    void blockChainingTest() {
+        BpmnModelInstance modelInstance =
+                new BPMNModelBuilder().block1().block2().block3().buildWithEndEvent();
 
-    assertThat(modelInstance.getModelElementsByType(FlowElement.class).size(), is(27));
-  }
+        assertThat(modelInstance.getModelElementsByType(FlowElement.class).size(), is(27));
+    }
 
-  @Test
-  void numberOfBlocksTest() {
-    BpmnModelInstance modelInstance1 = BPMNModelBuilder.createModelWithXBlocks(3);
-    assertThat(modelInstance1.getModelElementsByType(FlowElement.class).size(), is(27));
+    @Test
+    void numberOfBlocksTest() {
+        BpmnModelInstance modelInstance1 = BPMNModelBuilder.createModelWithXBlocks(3);
+        assertThat(modelInstance1.getModelElementsByType(FlowElement.class).size(), is(27));
 
-    BpmnModelInstance modelInstance2 = BPMNModelBuilder.createModelWithXBlocks(10);
-    assertThat(modelInstance2.getModelElementsByType(FlowElement.class).size(), is(81));
-  }
+        BpmnModelInstance modelInstance2 = BPMNModelBuilder.createModelWithXBlocks(10);
+        assertThat(modelInstance2.getModelElementsByType(FlowElement.class).size(), is(81));
+    }
 
-  //  @Test
-  void upToNumberOfBlocksTest() {
-    BPMNModelBuilder.createModelsWithUpToXBlocks(300);
-    assertTrue(true);
-  }
+    //  @Test
+    void upToNumberOfBlocksTest() {
+        BPMNModelBuilder.createModelsWithUpToXBlocks(300);
+        assertTrue(true);
+    }
 
-  //  @Test
-  void printStats() throws IOException {
-    BPMNStatPrinter.printStats(Path.of("C:/Source/scalability/"));
-  }
+    //  @Test
+    void printStats() throws IOException {
+        BPMNStatPrinter.printStats(Path.of("C:/Source/scalability/"));
+    }
 
-  //  @Test
-  void parallelismDegreesTest() {
-    ParallelismDegreeGenerator parallelismDegreeGenerator = new ParallelismDegreeGenerator();
-    IntStream.rangeClosed(1, 30).forEach(parallelismDegreeGenerator::generateParallelModel);
-  }
+    //    @Test
+    void parallelismDegreesTest() {
+        try (ForkJoinPool forkJoinPool = ForkJoinPool.commonPool()) {
+            IntStream.rangeClosed(1, 20).forEach(i -> IntStream.rangeClosed(1, 20).forEach(j -> forkJoinPool.execute(() -> {
+                ParallelBranchModelGenerator parallelBranchModelGenerator = new ParallelBranchModelGenerator();
+                parallelBranchModelGenerator.generateParallelBranchModel(i, j);
+            })));
+            assertTrue(forkJoinPool.awaitQuiescence(10, TimeUnit.MINUTES));
+        }
+    }
 }
